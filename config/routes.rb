1373e3b86ab88f1c36f4012d690812f47a210c23
Sam Worrall <sam.worrall@yourgolftravel.com>
2019-03-08 14:52:41 +0000

    Make adventures#home the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'adventures/home'
   get 'welcome/index'
-  root 'welcome#index'
+  root 'adventures#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
